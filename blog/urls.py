feab9d405ae420d483aff29c90a04da73bb67bb6
hanhyeju <hyevely1209@naver.com>
2023-11-22 14:39:54 +0900

    Update HTML

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.list),
     path('<int:no>/', views.detail),
     path('test1/', views.test1),
-    path('test2/<int:no>/', views.test2)
+    path('test2/<int:no>/', views.test2),
+    path('profile/', views.profile),
+    path('tag/<id>/', views.tag_list),
+    path('test3/', views.test3),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import *
 
 # 뷰객체: 서비스 처리 객체
 def test1(request):
@@ -16,4 +16,20 @@ def list(request):
 
 def detail(request, no):
     post = get_object_or_404(Post, id=no)
-    return render(request, 'blog/detail.html', {'post':post})
+    comment_list = post.comments.all()
+    tag_list = post.tag.all()
+    return render(request, 'blog/detail.html', {'post':post, 
+                                                'comment_all':comment_list, 
+                                                'tag_list':tag_list,})
+
+def profile(request):
+    user = User.objects.first()
+    return render(request, 'blog/profile.html', {'user':user})
+
+def tag_list(request, id):
+    tag = Tag.objects.get(id=id)
+    post_list = tag.post_set.all()
+    return render(request, 'blog/list.html', {'post_all':post_list})
+
+def test3(request):
+    return render(request, 'blog/form_test.html')
