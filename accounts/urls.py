8186a7d31a691fcc49ef9b3fe970af4b8317f9bc
hanhyeju <hyevely1209@naver.com>
2023-11-27 11:09:48 +0900

    End

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    #path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/', views.MyPasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('profile', TemplateView.as_view(template_name='registration/profile.html'), name='profile'),
 ]
